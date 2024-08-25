fecf3a5a1fe8d1603cb58e2d82b306bd7042388a
Ismanol García <dev@ismanolgarcia.com>
2024-08-24 23:06:00 -0400

    feat: add card

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        redHatText: 'Red Hat Text',
+      },
+      colors: {
+        Red: 'hsl(14, 86%, 42%)',
+        Green: ' hsl(159, 69%, 38%)',
+        Rose: {
+          50: 'hsl(20, 50%, 98%)',
+          100: 'hsl(13, 31%, 94%)',
+          300: 'hsl(14, 25%, 72%)',
+          400: 'hsl(7, 20%, 60%)',
+          500: 'hsl(12, 20%, 44%)',
+          900: 'hsl(14, 65%, 9%)',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
